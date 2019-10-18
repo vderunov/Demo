b237cb3ebfa93792cf49d868885ceaa68e78e647
Nikolo <voropn@gmail.com>
2019-10-18 18:48:06 +0300

    update components

--- shelter/src/app/app.module.ts
+++ shelter/src/app/app.module.ts
@@ -3,27 +3,30 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SheltersComponent } from './shelters/shelters.component';
-import { RegistrationComponent } from './registration/registration.component';
 import { LoginComponent } from './login/login.component';
 import { UsersComponent } from './users/users.component';
 import { MainService } from './shared/main.service';
 import { RegistrationShelterComponent } from './registration-shelter/registration-shelter.component';
 import { RegistrationUserComponent } from './registration-user/registration-user.component';
 import { NeedsComponent } from './needs/needs.component';
+import { SearchComponent } from './search/search.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
     AppComponent,
     SheltersComponent,
-    RegistrationComponent,
     LoginComponent,
     UsersComponent,
     RegistrationShelterComponent,
     RegistrationUserComponent,
-    NeedsComponent  ],
+    NeedsComponent,
+    SearchComponent,
+  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [MainService],
   bootstrap: [AppComponent]
--- shelter/src/app/search/search.component.spec.ts
+++ shelter/src/app/search/search.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RegistrationComponent } from './registration.component';
+import { SearchComponent } from './search.component';
 
-describe('RegistrationComponent', () => {
-  let component: RegistrationComponent;
-  let fixture: ComponentFixture<RegistrationComponent>;
+describe('SearchComponent', () => {
+  let component: SearchComponent;
+  let fixture: ComponentFixture<SearchComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegistrationComponent ]
+      declarations: [ SearchComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RegistrationComponent);
+    fixture = TestBed.createComponent(SearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
