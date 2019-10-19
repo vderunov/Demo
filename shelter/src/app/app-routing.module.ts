10fd0ceb2aa5c721fdece3cc3e3c90cab654967c
Volodymyr_Derunov <vderunov@gmail.com>
2019-10-20 02:13:39 +0300

    Create registration-shelter

--- shelter/src/app/app-routing.module.ts
+++ shelter/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { UsersComponent } from './users/users.component';
 import { SheltersComponent } from './shelters/shelters.component';
 import { LoginComponent } from './login/login.component';
@@ -9,17 +10,17 @@ import { NeedsComponent } from './needs/needs.component';
 import { SearchComponent } from './search/search.component';
 
 const routes: Routes = [
-  {path: 'needs', component: NeedsComponent},
-  {path: 'users', component: UsersComponent},
-  {path: 'shelters', component: SheltersComponent},
-  {path: 'search', component: SearchComponent},
-  {path: 'login', component: LoginComponent},
-  {path: 'registraction-shelter', component: RegistrationShelterComponent},
-  {path: 'registraction-user', component: RegistrationUserComponent},
+  { path: 'needs', component: NeedsComponent },
+  { path: 'users', component: UsersComponent },
+  { path: 'shelters', component: SheltersComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'registraction-shelter', component: RegistrationShelterComponent },
+  { path: 'registraction-user', component: RegistrationUserComponent }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- shelter/src/app/registration-shelter/registration-shelter.component.ts
+++ shelter/src/app/registration-shelter/registration-shelter.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { Shelter } from '../shared/interfaces';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration-shelter',
@@ -6,10 +10,40 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration-shelter.component.scss']
 })
 export class RegistrationShelterComponent implements OnInit {
+  form: FormGroup;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit() {
+    this.form = new FormGroup({
+      name: new FormControl('', Validators.required),
+      phone: new FormControl('', Validators.required),
+      address: new FormControl('', Validators.required),
+      kids: new FormControl('', Validators.required),
+      info: new FormControl('')
+    });
   }
 
+  submit() {
+    if (this.form.invalid) {
+      return;
+    } else {
+      // form data
+      const formData = { ...this.form.value };
+      this.form.reset();
+    }
+
+    // to work with the server in the future
+    const shelter: Shelter = {
+      name: this.form.value.name,
+      phone: this.form.value.phone,
+      address: this.form.value.address,
+      kids: this.form.value.kids,
+      info: this.form.value.info
+    };
+  }
+
+  gotoMainPage() {
+    this.router.navigate(['/']);
+  }
 }
