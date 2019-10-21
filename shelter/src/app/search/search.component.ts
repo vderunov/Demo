7b977bf81db1ccb82a4289c417f457920e972262
Nikolaenko M <nikomepf131@gmail.com>
2019-10-21 16:48:13 +0300

    Added search to Needs

--- shelter/src/app/search/search.component.ts
+++ shelter/src/app/search/search.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { SearchService } from './search.service';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
   styleUrls: ['./search.component.scss']
 })
+
+
+
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  @Input() searchTypes:  Array<String>;
+  @Input() list: Array<Object>;
+
+  public searchValue: String;
+
+  constructor(private searchService: SearchService) { }
 
   ngOnInit() {
+  }
+
+  public search(): void {
+    this.getFilteredDataBySearch();
+  }
+
+  public getFilteredDataBySearch() {
+    this.searchService.getFilteredDataBySearch(this.list, this.searchValue, this.searchTypes);
   }
 
 }
